0ee4381fcd8b1dd0311b76a5c2790cc49e6bb881
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-01-25 18:32:07 +0530

    ,

--- arrayupdate.java
+++ arrayupdate.java
@@ -2,7 +2,7 @@
 // public class arrayupdate{
 //     public static void update(int arr[]){
 //         for(int i=0; i<arr.length; i++){
-//             arr[i] = arr[i]+1;
+/             arr[i] = arr[i]+1;
 //         }
 //     }
 //     public static void main(String[] args){
