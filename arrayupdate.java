ac530d3fc852b87df4600d62ed2fc67298291ae6
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-01-27 18:35:55 +0530

    s

--- arrayupdate.java
+++ arrayupdate.java
@@ -13,6 +13,7 @@
 //         for(int i=0;i<arr.length;i++){
 //             System.out.println(arr[i]+" ");
 //         }
-/         System.out.println();
+//
+         System.out.println();
 //     }
 // }
