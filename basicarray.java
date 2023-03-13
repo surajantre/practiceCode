8c5c31ae8e41a6cd75994892d6337bdbaa95ee4c
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-03-13 18:42:53 +0530

    ,

--- basicarray.java
+++ basicarray.java
@@ -40,11 +40,11 @@
 //     public static void main(String[] args){
 //         int arr[]= new int[100];
 //         Scanner sc = new Scanner (System.in);
-/         arr[0]= sc.nextInt();
+//         arr[0]= sc.nextInt();
 //         arr[1]= sc.nextInt();
 //         arr[2]= sc.nextInt();
 
-//         System.out.println("phy"+arr[0]);
+/         System.out.println("phy"+arr[0]);
 //         System.out.println("phy"+arr[1]);
 //         System.out.println("phy"+arr[2]);
 //     }
