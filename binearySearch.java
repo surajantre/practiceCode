0a31a275f0e7df36d680fb3a9573282c6a947caf
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-03-23 18:40:44 +0530

    ,

--- binearySearch.java
+++ binearySearch.java
@@ -51,7 +51,7 @@
 //     }
 //     public static void main(String[] args){
 //         int arr[]={1,2,3,4,5,6,7};
-/         int key =5;
+//         int key =5;
 //         System.out.println(bs(arr, key));
 
 //     }
--- countingsort.java
+++ countingsort.java
@@ -4,7 +4,7 @@
 //         for(int i = 0;i<arr.length;i++) {
 //             largest = Math.max(largest,arr[i]);
 //         }
-//         int count [] = new int [largest+1];
+/         int count [] = new int [largest+1];
 //         for(int i = 0;i<arr.length;i++){
 //             count[arr[i]]++;
 //         }
