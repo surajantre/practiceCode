d1d4224611e6637b1d9707ee9dc8e8a245faed78
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-03-31 18:41:25 +0530

    ,

--- basicarray.java
+++ basicarray.java
@@ -40,7 +40,7 @@
 //     public static void main(String[] args){
 //         int arr[]= new int[100];
 //         Scanner sc = new Scanner (System.in);
-//         arr[0]= sc.nextInt();
+/         arr[0]= sc.nextInt();
 //         arr[1]= sc.nextInt();
 //         arr[2]= sc.nextInt();
 
--- countingsort.java
+++ countingsort.java
@@ -4,7 +4,7 @@
 //         for(int i = 0;i<arr.length;i++) {
 //             largest = Math.max(largest,arr[i]);
 //         }
-/         int count [] = new int [largest+1];
+//         int count [] = new int [largest+1];
 //         for(int i = 0;i<arr.length;i++){
 //             count[arr[i]]++;
 //         }
@@ -24,7 +24,7 @@
 //         int arr[] = {1,2,3,2,1,3,4,3,2,1,3,3,2,1};
 //         count(arr);
         
-//         for(int i = 0;i<arr.length;i++){
+/         for(int i = 0;i<arr.length;i++){
 //             System.out.println(arr[i] + " ");
 //         }
 
