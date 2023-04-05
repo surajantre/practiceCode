78a008e5f93078c94ff09f0a870150fcc9e62f12
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-05 10:02:14 +0530

    ,

--- 1/advancepattern3.java
+++ 1/advancepattern3.java
@@ -2,7 +2,7 @@
 //     public static void inverted_roteted_half_pyramidnumber(int n){
 //         for(int i = 1; i <= n; i++){
 //             for(int j = 1; j <= n-i+1; j++){
-//                 System.out.print(j+" ");
+/                 System.out.print(j+" ");
 //             }
 //             System.out.println();
 //         }
