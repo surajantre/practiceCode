2eadd950487b9b6742d0fc01b9c8c8fe7e4e4516
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-06 10:03:01 +0530

    ,

--- 1/betterflypattern.java
+++ 1/betterflypattern.java
@@ -6,7 +6,7 @@
 //             }
 //             for (int j = 1; j <=2*(n-i); j++){
 //                 System.out.print(" ");
-//             }
+//            }
 //              for (int j = 1; j <= i; j++){
 //                 System.out.print("*");
 //             }
