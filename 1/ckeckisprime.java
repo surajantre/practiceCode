020a84344bf49a6b11237e15ede2febc9d484c53
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-08 10:04:02 +0530

    ,

--- 1/ckeckisprime.java
+++ 1/ckeckisprime.java
@@ -2,7 +2,7 @@
 //     public static boolean isprimecheck(int n){
 //        // boolean is prime = 2;
 //         for(int i= 2; i <=n-1; i++){
-//             if(n%i==0){
+/             if(n%i==0){
 //                 boolean given  = false;
 //                 break;
 //             }
--- 1/diamond_pattern.java
+++ 1/diamond_pattern.java
@@ -6,7 +6,7 @@
 //             }
 //             for (int j = 1; j <= (2*i)-1; j++) {
 //                 System.out.print("*");
-/             }
+//             }
 //             System.out.println();
 //         }
 //         for (int i = n; i >=1;i--){
