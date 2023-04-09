4c5c621123f470a08f73c03403b6b3bb83568383
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-09 10:06:58 +0530

    ,

--- priflixarrays.java
+++ priflixarrays.java
@@ -21,7 +21,7 @@
 //        System.out.println("maxsum is " + maxsum);
 
  
-//     }
+/     }
 //     public static void main(String[] args) {
 //          int arr[] = {2,3,4,5,6,7};
 //          pri(arr);
