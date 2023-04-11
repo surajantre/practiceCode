fa108b66e21f69e82ccfb5dd058d4b19370a0142
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-11 17:51:57 +0530

    ,

--- 1/bintodec.java
+++ 1/bintodec.java
@@ -7,7 +7,7 @@
 
 //     while(binNum>0){
 //         int LastDig =  binNum% 10;
- /        decNum = decNum + (LastDig+(int)Math.pow(2,pow));
+ //        decNum = decNum + (LastDig+(int)Math.pow(2,pow));
 //         pow++;
 //         binNum = binNum / 10;
 // }
@@ -28,7 +28,7 @@
 //     public static void btod(int binNum){
 //         int myNum = binNum;
 //         int pow = 0;
-//         int decNum = 0;
+/         int decNum = 0;
 
 //         while(binNum>0){
 //             int LastDig = binNum % 10;
