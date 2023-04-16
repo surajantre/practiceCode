9f584602dc7345777f3782004fe91e3b9dbc6223
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-16 12:42:47 +0530

    ,,

--- variables.java
+++ variables.java
@@ -1,9 +1,9 @@
-/*public class variables {
-    public static void main(String[] args) {
+// /*public class variables {
+//     public static void main(String[] args) {
        
-        System.out.println("* * * *");
-        System.out.println("* * *");
-        System.out.print("* *\n");
-        System.out.print("*");
-    }
-} 
+//         System.out.println("* * * *");
+//         System.out.println("* * *");
+//         System.out.print("* *\n");
+//         System.out.print("*");
+//     }
+// } 
