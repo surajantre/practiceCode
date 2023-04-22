63b6e49872604045bb6d805711a03cc3c3a3eddf
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-22 17:56:47 +0530

    ,,

--- 1/betterflypattern.java
+++ 1/betterflypattern.java
@@ -4,7 +4,7 @@
 //             for (int j = 1; j <= i; j++){
 //                 System.out.print("*");
 //             }
-/             for (int j = 1; j <=2*(n-i); j++){
+//             for (int j = 1; j <=2*(n-i); j++){
 //                 System.out.print(" ");
 //            }
 //              for (int j = 1; j <= i; j++){
@@ -36,7 +36,7 @@
 
 // public class betterflypattern{
 //     public static void be(int n){
-//         for (int i=1; i<=n; i++){
+/         for (int i=1; i<=n; i++){
 //             for(int j=1;j<=i;j++){
 //                 System.out.print("*");
 //             }
