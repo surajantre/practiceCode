659ac259e0c90cf36dcc08ff1836c2009692bc4c
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-04-30 18:00:35 +0530

    ,

--- 1/dectobin.java
+++ 1/dectobin.java
@@ -34,12 +34,12 @@
 //             int rem = n%2;
 //             binNum = binNum + (rem*(int)Math.pow(10,pow));
 //             pow++;
-/             n=n/2;
+//             n=n/2;
 
 //         }
 //         System.out.println("your binary number is " + binNum);
 //     }
-//     public static void main(String args[]){
+/     public static void main(String args[]){
 //         dtob(0);
 //      }
 //  }   
