46d3b18701a5b881455a2c1adef3175d88185905
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-05-04 18:03:47 +0530

    ,

--- 1/Hellow_rhambus.java
+++ 1/Hellow_rhambus.java
@@ -2,7 +2,7 @@
 //     public static void h_r(int n){
 //         for(int i=1; i<=n; i++){
 //             for(int j=1; j<=n-i; j++){
-//                 System.out.print(" ");
+/                 System.out.print(" ");
 //             }
 //             for(int j=1; j<=n;j++){
 //                 if(i==1||i==n||j==1||j==n){
@@ -15,7 +15,7 @@
 //         }
 //     }
 //     public static void main(String[] args){
-/         h_r(5);
+//         h_r(5);
 
 //     }
 // }
