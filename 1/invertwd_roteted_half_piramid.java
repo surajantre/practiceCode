3c828d000348e8012ea10d9796e59419e59f4d9b
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-05-05 18:04:13 +0530

    ,

--- 1/invertwd_roteted_half_piramid.java
+++ 1/invertwd_roteted_half_piramid.java
@@ -1,11 +1,11 @@
 //public class invertwd_roteted_half_piramid {
 
 //     public static void inverted_rotated_half_piramide(int n){
-//         for (int i =1; i <= n; i++){
+/        for (int i =1; i <= n; i++){
 //             for (int j = 1; j <= n-i; j++){
 //                 System.out.print(" ");
 
-/             }for(int j=1; j <= i;j++){
+//             }for(int j=1; j <= i;j++){
 //                 System.out.print("*");
 //             }
 //             System.out.println();
