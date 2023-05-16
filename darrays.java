824ecad0e3dfd982c16853957eb2e551e8c44289
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-05-16 18:28:45 +0530

    ,

--- darrays.java
+++ darrays.java
@@ -6,13 +6,13 @@
 
 //         Scanner sc = new Scanner(System.in);
 //         for (int i = 0; i <n; i++) {
-/             for (int j = 0; j < m; j++) {
+//             for (int j = 0; j < m; j++) {
 //                 matrix[i][j] = sc.nextInt();
 //             }
 //         }
 //         for (int i = 0; i < n; i++) {
 //             for (int j = 0; j < m; j++) {
-//                 System.out.println(matrix[i][j]+" ");
+/                 System.out.println(matrix[i][j]+" ");
 //             }
 //         }
      //    System.out.println();
