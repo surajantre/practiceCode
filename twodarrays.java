d2e911df8367ac36b70a65f3871afe3c2526978d
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-05-17 18:20:46 +0530

    ,

--- twodarrays.java
+++ twodarrays.java
@@ -3,7 +3,7 @@
 //     public static void main(String[] args) {
 //          int matrix[][]=new int[3][3];
 //          int n = matrix.length, m= matrix[0].length;
-//          Scanner sc = new Scanner(System.in);
+/          Scanner sc = new Scanner(System.in);
 //          for (int i=1; i<n; i++) {
 //             for (int j=1; j<m; j++) {
 //                 matrix[i][j] = sc.nextInt();
