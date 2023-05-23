7a72b78a3b00854e70819620587e0c811f5be375
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-05-23 18:24:08 +0530

    ,

--- pairsinarray.java
+++ pairsinarray.java
@@ -7,7 +7,7 @@
 //             }
 //         }
 //         System.out.println();
-//     }
+/     }
 //     public static void main(String[] args){
 //         int arr[] = {2,3,4,5,6,7,8};
 //         pairs(arr);
