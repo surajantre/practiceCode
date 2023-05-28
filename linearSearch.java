1bd2ade047d1ff90ec612b94846c47d085523788
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-05-28 18:25:43 +0530

    ,

--- linearSearch.java
+++ linearSearch.java
@@ -3,7 +3,7 @@
 //         for(int i=0;i<arr.length;i++){
 //             if (arr[i]==key){
 //                 return i;
-//             }
+/             }
 //         }
 //         return -1;
 //     }
