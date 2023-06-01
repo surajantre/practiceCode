cef27a935bec7167541b79bcb0c30dd5a7a2ae62
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-01 19:25:45 +0530

    .

--- Stringpalindrom.java
+++ Stringpalindrom.java
@@ -1,15 +1,15 @@
-public class Stringpalindrom{
-    public static Boolean ispalidrom(String str){
-        for (int i=0; i<str.length()/2; i++){
-            int n = str.length();
-            if(str.charAt(i) != str.charAt(n-1-i)){
-                return false;
-            } 
-        }
-        return true;
-    }
-    public static void main(String args []){
-        String str = "madam";
-        System.out.println(ispalidrom(str));
-    }
-}
+// public class Stringpalindrom{
+//     public static Boolean ispalidrom(String str){
+//         for (int i=0; i<str.length()/2; i++){
+//             int n = str.length();
+//             if(str.charAt(i) != str.charAt(n-1-i)){
+//                 return false;
+//             } 
+//         }
+//         return true;
+//     }
+//     public static void main(String args []){
+//         String str = "madam";
+//         System.out.println(ispalidrom(str));
+//     }
+// }
