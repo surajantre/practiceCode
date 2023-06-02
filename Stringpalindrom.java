898044dc9fb3f42fd81d172b7a8affe1a4eb74f6
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-02 19:42:25 +0530

    .

--- Stringpalindrom.java
+++ Stringpalindrom.java
@@ -13,3 +13,43 @@
 //         System.out.println(ispalidrom(str));
 //     }
 // }
+
+
+// public class Stringpalindrom{
+//     public static Boolean isvalid(String str){
+//         for(int i=0; i<str.length()/2; i++){
+//             int n = str.length();
+//             if(str.charAt(i) != str.charAt(n-1-i)){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static void main(String args[]){
+//         String str = "madam";
+//         System.out.println(isvalid(str));
+
+//     }
+
+// }
+
+
+// public class Stringpalindrom{
+//     public static Boolean isvalid(String xyz){
+//         for(int i=0; i<xyz.length()/2; i++){
+//             int n =xyz.length();
+//             if( xyz.charAt(i) != xyz.charAt(n-1-i) ){
+//                 return false;
+//             } 
+
+//         }
+//         return true;
+//     }
+//     public static void main(String args[]){
+//         String xyz = "asddsaa";
+//         System.out.println(isvalid(xyz));
+//     }
+
+// }
+
+
