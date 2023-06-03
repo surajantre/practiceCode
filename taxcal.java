1e5a08aa918d3f3e9beef5051ff8d12079522650
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-03 12:05:27 +0530

    ...

--- taxcal.java
+++ taxcal.java
@@ -94,25 +94,25 @@
 //     }
 // }
 
-import java.util.*;
-public class taxcal {
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int income = sc.nextInt();
-        int tax;
+// import java.util.*;
+// public class taxcal {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int income = sc.nextInt();
+//         int tax;
 
-        if(income<500000){
-             tax = 0;
+//         if(income<500000){
+//              tax = 0;
         
-        }else if(income<= 500000 && income <= 1000000){
-             tax = (int) (income * 0.2);
-        } else{
-             tax = (int) (income * 0.3);
-        }
+//         }else if(income<= 500000 && income <= 1000000){
+//              tax = (int) (income * 0.2);
+//         } else{
+//              tax = (int) (income * 0.3);
+//         }
 
-        System.out.println("your tax is " +tax);
+//         System.out.println("your tax is " +tax);
 
-    }
+//     }
 
-}
+// }
 
