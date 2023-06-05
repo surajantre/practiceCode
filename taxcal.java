b2b525bb8fcfeec0549f9fe48d7a5b0e4383fcc5
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-05 12:02:17 +0530

    .

---
+++ taxcal.java
@@ -0,0 +1,96 @@
+// import java.util.*;
+// public class taxcal {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner (System.in);
+//         int income = sc.nextInt();
+//         int tax;
+
+//         if (income<500000){
+//             tax = 0;
+//         }
+//         else if (income>=500000 && income<1000000){
+//             tax = (int) (income * 0.2);
+        
+//         }else{
+//             tax = (int) (income * 0.3);
+//         }
+//         System.out.println("Your tax is = "+tax);
+
+//     }
+    
+// }
+
+// import java.util.Scanner;
+// public class taxcal{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int income = sc.nextInt();
+//         int tax;
+
+//         if (income<500000){
+//             tax = 0;
+//         }else if (income>=500000 && income<1000000){
+//             tax = (int) (income * 0.2);
+
+//         }else{
+//             tax = (int) (income*0.3);
+
+//         }
+//         System.out.println("Your tax is ="+tax);
+//     }
+// }
+
+
+
+
+
+
+
+// import java.util.*;
+// public class taxcal {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         int income = sc.nextInt();
+//         int tax;
+
+//         if(income<500000){
+//              tax = 0;
+        
+//         }else if(income<= 500000 && income <= 1000000){
+//              tax = (int) (income * 0.2);
+//         } else{
+//              tax = (int) (income * 0.3);
+//         }
+
+//         System.out.println("your tax is " +tax);
+
+//     }
+
+// }
+
+
+
+// import java.util.*;
+// public class taxcal{
+//     public static Boolean isvalid(String str){
+//         for(int i=0; i<str.length()/2; i++){
+//             int n = str.length();
+
+//         if(str.charAt(i)!= str.charAt(n-1-i)){
+//             return false;
+
+//         }
+
+        
+//         }
+//         return true;
+
+//         }
+
+    
+//     public static void main(String args[]){
+//         String str = "madama";
+//         System.out.println(isvalid(str));
+//     }
+// }
+
