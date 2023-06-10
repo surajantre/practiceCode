3c13f631a4a26724605b92d17cfca47db91f731c
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-10 14:43:16 +0530

    ...

---
+++ dowhile.java
@@ -0,0 +1,79 @@
+// public class dowhile {
+//     public static void main(String[] args){
+//         int n = 895682;
+//         while(n>0){
+//             int lastdigit = n%10;
+//             System.out.print(lastdigit);
+//             n=n/10;
+
+
+//         }
+//         System.out.println();
+//     }
+    
+// }
+
+
+
+// public class dowhile{
+//     public static void main(String[] args){
+//         int n =123456789;
+
+//         while(n>0){
+//             int lastdigit = n%10;
+//             System.out.print(lastdigit);
+//             n=n/10;
+//         }
+//         System.out.println();
+//     }
+
+// }
+
+
+
+
+
+
+// // multiplre of 10
+
+// import java.util.*;
+// public class dowhile{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner (System.in);
+//         do{
+//             System.out.println("enter your number");
+//             int n = sc.nextInt();
+
+//         if(n % 10==0){
+//                 continue; 
+//             }
+//             System.out.println("number was : " + n);
+
+//         }
+//         while(true);
+//     }
+
+// }
+
+
+
+
+
+
+// import java.util.*;
+// public class dowhile{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner (System.in);
+//         do{
+//             System.out.println("enter your number :");
+//             int n = sc.nextInt();
+
+//             if (n %10==0){
+//                 break;
+//             }
+//             System.out.println(n);
+
+//         }while(true);
+        
+//     }
+// }
--- printpatterns.java
+++ printpatterns.java
@@ -26,20 +26,20 @@
 
 // }
 
-import java.util.Scanner;
-public class printpatterns{
-    public static void main(String args[]){
-        Scanner sc = new Scanner (System.in);
-        int n = sc. nextInt();
-        int sum = 0;
+// import java.util.Scanner;
+// public class printpatterns{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner (System.in);
+//         int n = sc. nextInt();
+//         int sum = 0;
 
-        int i = 0;
-        while (i <=n){
-            sum += i;
-            i++;
-        }
-        System.out.println(sum);
+//         int i = 0;
+//         while (i <=n){
+//             sum += i;
+//             i++;
+//         }
+//         System.out.println(sum);
 
-    }
+//     }
 
-}
+// }
