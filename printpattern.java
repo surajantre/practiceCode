6c015edec3caa9e87c694647233e79b44dec02e2
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-12 14:12:20 +0530

    ..

--- printpattern.java
+++ printpattern.java
@@ -10,3 +10,39 @@
 //     }
 // }
 
+
+
+// public class printpattern{
+//     public static void main(String[] args){
+//         int x = 7;
+//         for(int i=1; i<=x; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+
+//     }
+// }
+
+
+
+
+// public class printpattern{
+//     public static void main(String[] args){
+//         int n = 4;
+//         char c = 'A';
+//         for(int i = 0; i < n; i++){
+//             for(int j = 0; j <=i; j++){
+//                 System.out.print(c);
+//                 c++;
+
+//             }
+//             System.out.println();
+//         } 
+
+
+//     }
+// }
+
+
