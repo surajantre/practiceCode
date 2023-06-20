3505a7bbb1fee3f39528763915441e28bf12fcd8
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-20 17:35:17 +0530

    .

---
+++ floyodstriangle.java
@@ -0,0 +1,34 @@
+// public class floyodstriangle {
+//     public static void triflo(int n){
+//         int count= 1;
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print(count+" ");
+//                 count++;
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String[] args){
+//         triflo(5);
+
+//     }
+// }
+
+
+// class floyodstriangle{
+//     public static void flotri(int n){
+//         int count = 1;
+//         for (int i=1; i<=n; i++){
+//             for(int j=1; j<=i; j++){
+//                 System.out.print(count+" ");
+//                 count++;
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String[] args){
+//         flotri(5);
+
+//     }
+ }
