8aaa0412abf1ab871d6d4037de258740e0d76ac0
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-21 17:36:04 +0530

    ..

---
+++ zeroonrtriangle.java
@@ -0,0 +1,41 @@
+// public class zeroonrtriangle {
+//     public static void onezerotri(int n){
+//         for (int i = 1; i <= n; i++){
+//             for (int j =1; j <= i; j++){
+//                 if((i+j)%2==0){
+//                     System.out.print("1");
+//                 }else{
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+        
+//         }
+//     }
+//     public static void main(String[] args){
+//         onezerotri(5);
+
+//     }
+// }
+
+// import java.net.SocketTimeoutException;
+
+// public class zeroonrtriangle{
+//     public static void onezerotri(int n){
+//         for (int i = 1; i <= n; i++){
+//             for (int j = 1; j <= i; j++){
+//                 if((i+j)%2==0){
+//                     System.out.print("1");
+
+//                 }else{
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     } 
+//     public static void main(String[] args){
+//         onezerotri(4);
+
+     }
+}
