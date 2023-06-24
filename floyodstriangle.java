bb6a51a52665af11cbea6abed75a190724009792
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-06-24 18:08:53 +0530

    .

--- floyodstriangle.java
+++ floyodstriangle.java
@@ -32,3 +32,21 @@
 
 //     }
  //}
+
+
+//  public class floyodstriangle{
+//     public static void flotri(int n){
+//        int  count =1;
+//        for (int i=1; i<=n; i++){
+//         for(int j=1; j<=i; j++){
+//             System.out.print(count+" ");
+//             count++;
+//         }
+//         System.out.println();
+//        }
+//     }
+//     public static void main(String[] args){
+//         flotri(4);
+
+//     }
+//  }
