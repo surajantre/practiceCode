6d4ba028e50fd2d68e213a747618d75c252af5d3
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-07-10 23:00:49 +0530

    ...

--- subarrays.java
+++ subarrays.java
@@ -36,7 +36,7 @@
 //         }
 //     }
 //     public static void main(String[] args){
-//         int arr[] ={2,3,4,5,6,7,8};
+/         int arr[] ={2,3,4,5,6,7,8};
 //         sub(arr);
 //     }
 // }
