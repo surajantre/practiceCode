6306e6c7d1a573990e3327a673c099cbb4e53ff7
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-15 16:00:33 +0530

    dhmf

--- boilerplateCode.java
+++ boilerplateCode.java
@@ -1,5 +1,21 @@
-public class boilerplatecode{
-    public static void main(String args[]){
-        System.out.println(".......");
-    }
-}
+// public class boilerplatecode{
+//     public static void main(String args[]){
+//         System.out.println(".......");
+        
+
+// public class fri1108 {
+//     public static void main(String[] args){
+//         String temp = "my name is suraj";
+
+//         System.out.println("String size = " + temp.length());
+
+        
+//             }
+    
+//         }
+    
+
+
+        
+//     }
+// }
