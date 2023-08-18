a01794d27d43df8f0c1148df69090d29e24c0765
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-18 17:00:35 +0530

    .

--- Stringpalindrom.java
+++ Stringpalindrom.java
@@ -20,3 +20,19 @@
 // }\
 
  
+public class Stringpalindrom{
+    public static Boolean ispalidrom(String str){
+        for (int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            } 
+        }
+        return true;
+
+    }
+    public static void main(String args []){
+        String str = "madam";
+        System.out.println(ispalidrom(str));
+    }
+}
--- boilerplateCode.java
+++ boilerplateCode.java
@@ -1,8 +1,10 @@
-// public class boilerplatecode{
-//     public static void main(String args[]){
-//         System.out.println(".......");
-        
+import java.util.*;
 
-//     }
-// }
-//
+public class boilerplatecode{
+    public static void main(String args[]){
+         Scanner sc = new Scanner (System.in);
+         float a = 24.23421f;
+         int b = (int)a;
+         System.out.println(b);
+    }
+}
