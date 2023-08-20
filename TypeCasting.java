a96b0ad453c900d117fe5f6c251581fa8cc2b27e
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-20 15:34:25 -0500

    20/08/2023

---
+++ TypeCasting.java
@@ -0,0 +1,8 @@
+public class TypeCasting {
+    public static void main(String args[]){
+        float num1 = 23.45436f;
+        int num2 =(int)num1;
+        System.out.println(num2); 
+}
+    
+}
