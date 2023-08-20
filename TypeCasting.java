6634fd471f9b1944709c834f778baea548d03b3a
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-20 15:50:24 +0530

    .

--- TypeCasting.java
+++
@@ -1,8 +0,0 @@
-public class TypeCasting {
-    public static void main(String args[]){
-        float num1 = 23.45436f;
-        int num2 =(int)num1;
-        System.out.println(num2); 
-}
-    
-}
---
+++ boilerplateCode.java
@@ -0,0 +1,5 @@
+public class boilerplatecode{
+    public static void main(String args[]){
+        
+    }
+}
