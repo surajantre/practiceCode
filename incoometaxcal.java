8bc501a7e906e606a738e54326988e6de5f3826c
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-23 17:48:15 +0530

    .

--- incoometaxcal.java
+++ incoometaxcal.java
@@ -12,9 +12,22 @@
 
 // }
 
- 
+//  import java.util.*;
 // public class incoometaxcal{
+    
+
+    
 //     public static void main(String[] args){
+//         Scanner sc= new Scanner (System.in);
+//         int number = sc.nextInt();
+
+//         if (number %2 == 0){
+//             System.out.println("Given number is Even");
+//         }   else{
+//             System.out.println("Given number is odd");
+//         }
+
+
         
 //     }
 
