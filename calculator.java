a2153cc5f2d26ba904ba38e3de8b6ee952b0a26c
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-26 13:57:43 +0530

    .

--- calculator.java
+++ calculator.java
@@ -27,27 +27,44 @@
 
 //         }
 //     }
+// //         }
+// import java.util.*;
+// public class calculator{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner (System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         char operator = sc.next().charAt(0);
+//         switch(operator){
+//             case '+':System.out.println(a+b);
+//             break;
+//             case '-':System.out.println(a-b);
+//             break;
+//             case '*':System.out.println(a*b);
+//             break;
+//             case '/':System.out.println(a/b);
+//             break;
+//             case '%':System.out.println(a%b);
+//             break;
+//             default:System.out.println("wrong operator");
+
 //         }
+//     }
+// }
+
+
 import java.util.*;
 public class calculator{
-    public static void main(String[] args){
-        Scanner sc = new Scanner (System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        char operator = sc.next().charAt(0);
-        switch(operator){
-            case '+':System.out.println(a+b);
-            break;
-            case '-':System.out.println(a-b);
-            break;
-            case '*':System.out.println(a*b);
-            break;
-            case '/':System.out.println(a/b);
-            break;
-            case '%':System.out.println(a%b);
-            break;
-            default:System.out.println("wrong operator");
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);;
+        int n = sc.nextInt();
+        int count = 1;
 
-        }
+    while(count <=n){
+        System.out.println(count +" ");
+        count++;
+
+    }
+    System.out.println();
     }
 }
