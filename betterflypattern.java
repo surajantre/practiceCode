dea4347f983cbdddb23dc4bd4fd8d657b59fb95b
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-30 17:34:09 +0530

    ,

---
+++ betterflypattern.java
@@ -0,0 +1,71 @@
+// public class betterflypattern {
+//     public static void bettrfiy(int n){
+//         for (int i = 1; i <= n; i++){
+//             for (int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             for (int j = 1; j <=2*(n-i); j++){
+//                 System.out.print(" ");
+//             }
+//              for (int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         for(int i=n; i >= 1; i--){
+//              for (int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             for (int j = 1; j <=2*(n-i); j++){
+//                 System.out.print(" ");
+//             }
+//              for (int j = 1; j <= i; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+        
+
+//         }
+//     }
+//     public static void main(String[] args){
+//         bettrfiy(4);
+
+//     }
+// }
+
+
+// public class betterflypattern{
+//     public static void be(int n){
+//         for (int i=1; i<=n; i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             for (int j=1;j<=2*(n-i);j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+            
+//         }
+//         for (int i=n;i>=1;i--){
+//                 for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             for (int j=1;j<=2*(n-i);j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+            
+        
+//         }
+//     }
+//     public static void main(String[] args){
+//         be(5);
+
+//     }
+// }
