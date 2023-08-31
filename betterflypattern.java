381cd903e71b93c5fcd5f843eea40cc4c47dba51
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-08-31 18:07:21 +0530

    ,

--- betterflypattern.java
+++ betterflypattern.java
@@ -69,3 +69,58 @@
 
 //     }
 // }
+
+// public class betterflypattern{
+//     public static void bet(int n){
+//         for(int i=0;i<=n;i++){
+//             for(int j=0;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             for(int j=0;j<=2*(n-i);j++){
+//                 System.out.print(" ");
+//             }
+//              for(int j=0;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+
+//         }
+//         for(int i=n-1;i>=1;i--){
+//             for(int j=0;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             for(int j=0;j<=2*(n-i);j++){
+//                 System.out.print(" ");
+//             }
+//              for(int j=0;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main(String[] args){
+//         bet(5);
+
+//     }
+// }
+
+// import javax.sound.sampled.SourceDataLine;
+
+// class betterflypattern{
+//     public static void ca(int n){
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 if((i+j)%2==0){
+//                     System.out.print("1");
+//                 }else{
+//                     System.out.print("0");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+//     public static void main (String[] args){
+//         ca(5);
+
+//     }
+// }
---
+++ solid_rhombus.java
@@ -0,0 +1,20 @@
+// public class solid_rhombus {
+//     public static void s_r(int n){
+//         for(int i = 1; i <= n; i++){
+//             for(int j = 1; j <= n-i; j++){
+//                 System.out.print(" ");
+                
+
+//             }
+//             for(int j=1; j <= n; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+
+//         }
+//     }
+//     public static void main(String[] args){
+//         s_r(5);
+
+//     }
+// }
