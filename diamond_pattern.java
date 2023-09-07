34c35fd0edf6bd2ed94d3473e5fd1dbb80906d52
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-09-07 14:15:12 +0530

    ,,,,,,

--- diamond_pattern.java
+++ diamond_pattern.java
@@ -36,7 +36,7 @@
 
 //             }
 //             for (int j = 1; j <=(2*i)-1 ;j++){
-//                 System.out.print("*");
+                    System.out.print("*");
 //             }
 //             System.out.println();
 
