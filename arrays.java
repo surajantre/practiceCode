8a85875650f63e8a625249b4cb9ccf77b752bcd6
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-09-11 22:26:58 +0530

    .

---
+++ arrays.java
@@ -0,0 +1,36 @@
+// import java.util.*;
+// public class arrays {
+    
+//     public static void main(String args[]){
+//         int array[]=new int [100];
+//         Scanner sc = new Scanner(System.in);
+
+//          array[0]=sc.nextInt();
+//          array[1]=sc.nextInt();
+//          array[2]=sc.nextInt();
+
+//          System.out.println("phy:"+array[0]);
+//          System.out.println("chem:"+array[1]);
+//          System.out.println("bio:"+array[2]);
+
+//     }
+// }
+
+// import java.util.*;
+// class arrays{
+//     public static void update(int marks[]){
+//         for(int i=0; i<marks.length; i++){  
+//             marks[i] = marks[i]+1;
+//         }
+//     }
+//     public static void main(String[] args){
+//         int marks[]={90,93,84,0};
+//         update(marks);
+
+//         for(int i=0; i<marks.length; i++){
+//             System.out.print(marks[i]+" ");
+
+//         }
+//         System.out.println();
+//     }
+// }
--- prac.java
+++ prac.java
@@ -1,5 +1,33 @@
-public class prac {
-    public static void main(String[] args){
-        System.out.println(" starting");
-    }
-}
+// class prac {
+//     public static void pattern(int n){
+//         for(int i=1; i<=n; i++){
+//             for(int j=1; j<=n-i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1; j<=(2*i)-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+
+//         }
+//         for(int i=n; i>=0; i--){
+//             for(int j=1; j<=n-i; j++){
+//                 System.out.print(" ");
+//             }
+//             for(int j=1; j<=(2*i)-1; j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         //System.out.println();
+//     }
+//     public static void main(String[] args){
+//         pattern(4);
+        
+//     }
+// }
+
+
+
+
+
