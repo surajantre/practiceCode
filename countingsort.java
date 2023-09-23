5ccaac0c5e38c1c14406b5a68b704659cb646286
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-09-23 18:09:29 +0530

    .

--- countingsort.java
+++ countingsort.java
@@ -1,5 +1,32 @@
-public class countingsort {
-    public static void main(String[] args) {
+// public class countingsort {
+//     public static void count(int arr[]){
+//         int largest = Integer.MIN_VALUE;
+//         for(int i = 0;i<arr.length;i++) {
+//             largest = Math.max(largest,arr[i]);
+//         }
+//         int count [] = new int [largest+1];
+//         for(int i = 0;i<arr.length;i++){
+//             count[arr[i]]++;
+//         }
+//         //sorting j=0;
+//         for(int i = 0;i<arr.length;i++) {
+//             while  ( count[i]>0){
+//                 arr[i] = i;
+//                 i++;
+//                 count[i]--;
+
+//             }
+//         }
+
+//     }
+
+//     public static void main(String[] args) {
+//         int arr[] = {1,2,3,2,1,3,4,3,2,1,3,3,2,1};
+//         count(arr);
         
-    }
-}
+//         for(int i = 0;i<arr.length;i++){
+//             System.out.println(arr[i] + " ");
+//         }
+
+//     }
+// }
---
+++ twodarrays.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+// class twodarrays{
+//     public static void main(String[] args) {
+//          int matrix[][]=new int[3][3];
+//          int n = matrix.length, m= matrix[0].length;
+//          Scanner sc = new Scanner(System.in);
+//          for (int i=1; i<n; i++) {
+//             for (int j=1; j<m; j++) {
+//                 matrix[i][j] = sc.nextInt();
+//             }
+//          }
+//          for (int i=1; i<n; i++) {
+//             for (int j=1; j<m; j++) {
+//                 System.out.print(matrix[i][j]+" ");
+//             }
+//             System.out.println();
+//          }
+//     }
+// }
