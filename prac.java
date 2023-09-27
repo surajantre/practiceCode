4efb1915cf1be3f2dc3a9d64bf4d13f819c170fa
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-09-27 20:27:07 +0530

    ,

--- prac.java
+++ prac.java
@@ -7,7 +7,7 @@
 //             for(int j=1; j<=(2*i)-1; j++){
 //                 System.out.print("*");
 //             }
-//             System.out.println();
+             System.out.println();
 
 //         }
 //         for(int i=n; i>=0; i--){
