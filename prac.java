524954245732de2a6d28038e45d10d5b43f158dc
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-09-29 18:45:48 +0530

    ,

--- prac.java
+++ prac.java
@@ -4,7 +4,7 @@
 //             for(int j=1; j<=n-i; j++){
 //                 System.out.print(" ");
 //             }
-/             for(int j=1; j<=(2*i)-1; j++){
+//             for(int j=1; j<=(2*i)-1; j++){
 //                 System.out.print("*");
 //             }
 //             System.out.println();
