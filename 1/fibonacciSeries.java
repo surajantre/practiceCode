a40a5f6d05a0b01bf0db956eb58b559e846ffda0
SURAJ ANTRE <120030250+SurajAntre7777@users.noreply.github.com>
2023-10-02 19:36:53 +0530

    ,

--- 1/fibonacciSeries.java
+++ 1/fibonacciSeries.java
@@ -1,14 +1,32 @@
-public class fibonacciSeries{
-    public static void main(String[] args) {
-        int a=0;
-        int b=1;
-        int c;
-        for (int i=1; i<=10; i++){
-            c= a+b;
-            System.out.print(c+" ");
-            a=b;
-            b=c; 
+// public class fibonacciSeries{
+//     public static void main(String[] args) {
+//         int a=0;
+//         int b=1;
+//         int c;
+//         for (int i=1; i<=10; i++){
+//             c= a+b;
+//             System.out.print(c+" ");
+//             a=b;
+//             b=c; 
 
-        }
-    }
-}
+//         }
+//     }
+// }
+
+
+
+
+//public class fibonacciSeries{
+    //     public static void main(String[] args) {
+    //         int a=0;
+    //         int b=1;
+    //         int c;
+    //         for (int i=1; i<=10; i++){
+    //             c= a+b;
+    //             System.out.print(c+" ");
+    //             a=b;
+    //             b=c; 
+    
+    //         }
+    //     }
+    // }
